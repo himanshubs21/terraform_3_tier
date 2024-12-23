1ca23afc52dc35b0de13304cffe9c4d5c057a746
Himanshu Bhushan <155674846+himanshubs21@users.noreply.github.com>
2024-12-23 18:58:45 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 
 variable "static_website_bucket_name" {
   type    = string
-  default = "static_website-hb-241224"
+  default = "static-website-hb-241224"
 }
 variable "build-bucker-name" {
   type    = string
